db1483306621dafb631693401e0097d1578db663
James Mernin <jamespmernin@gmail.com>
2020-09-16 12:49:56 -0400

    Finished algorithm

--- palindrome.js
+++ palindrome.js
@@ -1,5 +1,5 @@
 const palindrome = str => {
-
+  return str.toLowerCase().replace(/[\W_]/g, '') === str.toLowerCase().replace(/[\W_]/g, '').split('').reverse().join(''); // source: https://itnext.io/11-way-to-check-for-palindromes-in-javascript-85dbfe7dfb5d
 }
 
 console.log(palindrome("a man a plan a canal panama"))
